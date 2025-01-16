f239a3f8b774d8dc342c40a1055c3251861f1872
Bryant Mejia <bmejia220@gmail.com>
2025-01-16 11:52:16 -0800

    deployment .venv

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 python3 -m pip install --upgrade pip
+
+python3 -m venv .venv
+source activate ./.venv/bin/activate
+pip3 install -r requirements-prod.txt
+
 cd backend
 
 echo "${0}: running migrations."
