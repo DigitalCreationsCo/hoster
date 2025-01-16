f0519c2a3cee5dd404ae2b7627218c0197319347
Bryant Mejia <bmejia220@gmail.com>
2025-01-16 11:09:46 -0800

    action: entrypoint file

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 python3 -m pip install --upgrade pip
 echo "${0}: running migrations."
 python3 manage.py migrate
-python3 manage.py loaddata seed_data.json
+# python3 manage.py loaddata seed_data.json
 python3 manage.py collectstatic --no-input
 python3 -m gunicorn project.wsgi:application
